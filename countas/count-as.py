60843c39daeb53d3947098198921bd18d7dc20e7
nemeth-bence <nemeth.bence@alwt.hu>
2017-04-10 11:38:55 +0200

    Seconds exercise refactored - done

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,22 @@
-def count_as(change_this):
-    pass
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
+
+file = open("afile.txt" , "r")
+text = file.read()
+file.close
+
+def count_as(filename):
+    try:
+        text = open(filename, 'r')
+        number_of_as = 0
+        for letter in text:
+            if letter =='a' or letter == 'A':
+                number_of_as += 1
+        return number_of_as
+    except FileNotFoundError:
+        print('0')
+
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
